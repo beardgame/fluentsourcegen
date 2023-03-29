b6ebcf2fb600c1acb23143134fb894d727bca956
William Barbosa <heytherewill@gmail.com>
2023-03-29 14:40:25 -0300

    2️⃣ Add netstandard2.0 support (#87)
    * 2️⃣ Add netstandard2.0 support
    
    * 🧪 Run tests on netcore as well

---
+++ Bearded.FluentSourceGen.Tests/ModuleInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
+
+namespace System.Runtime.CompilerServices
+{
+    // HACK: Needed to make ModuleInitializer work
+    [ExcludeFromCodeCoverage]
+    [DebuggerNonUserCode]
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    public sealed class ModuleInitializerAttribute : Attribute { }
+}
---
+++ Bearded.FluentSourceGen/IsExternalInit.cs
@@ -0,0 +1,5 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    // HACK: This is needed in order to use C#9+ features in netstandard
+    public class IsExternalInit { }
+}
