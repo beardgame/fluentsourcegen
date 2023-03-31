8511c1549b8a502541215c6505863ab798d61d05
Tom Rijnbeek <tom@tomrijnbeek.me>
2023-03-31 15:36:39 +0200

    🔥 Remove scratch comment

--- Bearded.FluentSourceGen/Core/ClassBuilder.cs
+++ Bearded.FluentSourceGen/Core/ClassBuilder.cs
@@ -1,17 +1,4 @@
 namespace Bearded.FluentSourceGen;
-/*
-
-NewClassBuilder()
-.AddField<int>("myInt", out var intFieldReference)
-.AddField<string>("myString", out var stringFieldReference)
-.AddField<bool>("derivedBool", out var boolFieldReference)
-.AddConstructor(ctor => ctor
-.InjectFields(intFieldReference, stringFieldReference)
-.AddParameter<double>("myDouble", out _)
-.AssignField(boolFieldReference, Expression.FromString("myDouble > 0.5")))
-.ToSourceString();
-
- */
 
 public sealed partial class ClassBuilder
 {
